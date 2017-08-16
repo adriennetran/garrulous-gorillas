73a9266a19dd07688dd70de5e2052096027d5522
Adrienne Tran <adrienne@adriennetran.com>
2017-08-16 11:30:19 -0400

    Debate Item

--- client/src/components/DebateItem.jsx
+++ client/src/components/DebateItem.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import {
-  Link
+	Link
 } from 'react-router-dom';
 
 const DebateItem = (props) => {
 
-  return (
-    <li className="debate-item-style">
-    	<Link to={`debates/${props.debate.topic.replace(/\s+/g, '-').replace(/[^a-zA-Z0-9-]/g, '').toLowerCase()}`} onClick={()=>props.debateSelectHandler(props.debate)}>
-      {props.debate.topic }
-    	</Link>
-  	</li>
-  )
+	return (
+		<li className="debate-item-style">
+			<Link to={`debates/${props.debate.topic.replace(/\s+/g, '-').replace(/[^a-zA-Z0-9-]/g, '').toLowerCase()}`} onClick={()=>props.debateSelectHandler(props.debate)}>
+				{props.debate.topic }
+			</Link>
+		</li>
+	)
 }
 
 export default DebateItem;
